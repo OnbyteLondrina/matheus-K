17cc2d3277c444e44bda7d809ad050e4670ae97e
OnbyteLondrina <alunoonbytelondrina@gmail.com>
2023-03-28 19:19:02 -0300

    subtração1

--- aula_8/recebendoDados.js
+++ aula_8/recebendoDados.js
@@ -3,3 +3,9 @@ function soma(numero1, numero2){
     console.log(resultado);
 }
 soma(3,2);
+
+function subtrair(numero1,numero2){
+    return numero1 - numero2;
+}
+resultado = subtrair(8,4);
+console.log(resultado);
