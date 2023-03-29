99a30b8711f513b04e8984cfc776eb29ab6d0310
OnbyteLondrina <alunoonbytelondrina@gmail.com>
2023-03-29 20:05:42 -0300

    estruRepet continue

--- aula_11/estruturaRepeticao.js
+++ aula_11/estruturaRepeticao.js
@@ -20,3 +20,13 @@ do{
     console.log(j);
     j++;
 }while (j < 10);
+
+var k = 0;
+while (k < 10){
+    if (k === 2 || k === 4 || k === 6 || k === 8){
+        k++;
+        continue;
+    }
+    console.log(k);
+    k++;
+}
