d63202e2cbcd7e532f20b670547d30ef1ca624fe
OnbyteLondrina <alunoonbytelondrina@gmail.com>
2023-03-29 19:24:45 -0300

    calculadora 2

--- aula_10/calculadora.js
+++ aula_10/calculadora.js
@@ -1,3 +1,19 @@
 var numero1 = prompt("Digite o primeiro número:");
 var numero2 = prompt("Digite o segundo número:");
 var operacao = prompt("Digite a operação:");
+
+function calculadora(x, y, operacao){
+    if (operacao === "+"){
+        return parseInt(x) + parseInt(y);
+    }else if (operacao === "-"){
+        return parseInt(x) - parseInt(y);
+    }else if (operacao === "*"){
+        return parseFloat(x) * parseFloat(y);
+    }else if (operacao === "/"){
+        return parseFloat(x) / parseFloat(y);
+    }else{
+        return console.log("Não foi possível realizar a operação");
+    }
+}
+
+console.log(calculadora(numero1,numero2,operacao));
