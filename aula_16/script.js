96c6d1c52c3e7268a57e431b6429e44580f5c0dc
OnbyteLondrina <alunoonbytelondrina@gmail.com>
2023-03-30 20:29:55 -0300

    calculadora apagando

--- aula_16/script.js
+++ aula_16/script.js
@@ -7,3 +7,10 @@ function inserir(valor){
 function limpar(){
     RESULTADO.innerHTML = '';
 }
+
+function apagar(){
+    if(RESULTADO.textContent){
+        let remove = document.querySelector('.result').innerHTML;
+        RESULTADO.innerHTML = remove.substring(0, remove.length - 1);
+    }
+}
