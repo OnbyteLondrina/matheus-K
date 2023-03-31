c902efe4a8f95d128cea78aa45052e5efeb17b20
OnbyteLondrina <alunoonbytelondrina@gmail.com>
2023-03-31 19:40:12 -0300

    aula 18 , teste

---
+++ aula_18/main.js
@@ -0,0 +1,15 @@
+function enviar(){
+    var endEmail = document.querySelector('#email').value;
+    var senha = document.querySelector('#senha').value;
+
+    console.log(endEmail);
+    console.log(senha);
+
+    var lemb = document.querySelector('#checkbox');
+    if (lemb.checked){
+        console.log("Checkbox selecionada");
+    }
+    else{
+        console.log("Checkbox não selecionada");
+    }
+}
